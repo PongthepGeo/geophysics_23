fa7ce355efed79dc76dfb451f03a044232145587
PongthepGeo <pongtep_tong@hotmail.com>
2023-11-01 15:37:50 +0700

    Nov_01

--- codes/seismic/setup.py
+++ codes/seismic/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='seismic',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['seismic', 'seismic.*', 'seismic.Libs', 'seismic.Libs.*']),
     install_requires=[
         'deepwave'
     ],
@@ -12,7 +12,6 @@ setup(
     author_email='pongtep_tong@hotmail.com',
     description='A seismic library',
 )
-
 
 # pip install git+https://github.com/PongthepGeo/geophysics_23.git#subdirectory=codes/seismic
 
